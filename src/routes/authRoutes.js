5db4dbb6c2c87b2cc4d9f434c38705b72c8c21f2
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-31 16:05:32 +1000

    added new user submit error handling

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,14 @@ const router = express.Router();
 
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
+    try {
+        const user = new User({ email, password });
+        await user.save();
 
-    const user = new User({ email, password });
-    await user.save();
-
-    res.send('User saved')
+        res.send('User saved')
+    } catch (err) {
+        return res.status(422).send(err.message)
+    }
 });
 
 module.exports = router;
