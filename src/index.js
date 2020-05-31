81a8103849552400e83890e3aacab670a1daea49
Michael Antoniades <mic.antoniades@gmail.com>
2020-05-31 14:59:41 +1000

    mongodb connection plus config (local only)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require ('express');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
+
+console.log("mongo URI is: ", config.development.database.mongoUri);
+
+mongoose.connect(config.development.database.mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to mongo instance: ${mongoose.connection.name}`)
+});
+mongoose.connection.on('error', (err) => {
+    console.log(`Error connecting to mongo db: ${err}`)
+});
 
 app.get('/', (req, res) => {
     res.send('express')
