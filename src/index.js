b3c4a9d920714d05458e6b7dc6add235032d6ff0
Michael Antoniades <mic.antoniades@gmail.com>
2020-06-02 12:53:06 +1000

    added track route and get handler, & import track route to index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 require('./models/User');
+require('./models/Track');
 
 const express = require ('express');
 const mongoose = require('mongoose');
 const config = require('./config')
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(config.development.database.mongoUri, {
     useNewUrlParser: true,
